07d6d0a2d9ff5d8a2afa59ce7ef945fa46d1463b
xcezzz <xxcezz@gmail.com>
2014-05-05 14:33:07 -0400

    create favicons… trash svgs

--- build.theme.sh
+++ build.theme.sh
@@ -6,6 +6,8 @@ convert template.png -crop 270x30+60+60 png32:${LOGOS_DIR}/logo-wide.png
 
 convert template.png +repage -crop 270x42+60+121 +repage gif:${LOGOS_DIR}/logo-mail.gif
 convert template.png -crop 300x150+60+195 png32:${LOGOS_DIR}/logo.png
-convert ${LOGOS_DIR}/logo-wide.png ${LOGOS_DIR}/logo-wide.svg
-convert ${LOGOS_DIR}/logo.png ${LOGOS_DIR}/logo.svg
+
+convert template.png -crop 128x128+537+60 png32:${LOGOS_DIR}/favicon-touch.png
+convert ${LOGOS_DIR}/favicon-touch.png -resize 32x32 ${LOGOS_DIR}/favicon.png
+
 rm -Rf `find ./ -iname '.DS_Store'`
